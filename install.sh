3d48dc041a4cb6dcc9fd5971781bfb8df252c8ac
Omaam <omama.tomoki@gmail.com>
2024-05-02 08:46:51 +0900

    expand install capability

--- install.sh
+++ install.sh
@@ -1,78 +1,88 @@
-#!/bin/sh
-set -ue
+#!/bin/bash
 
-helpmsg() {
-  echo "Usage: $0 [--help | -h]" 0>&2
-  echo ""
+set_installer() {
+    if [[ "$(uname)" == "Linux" ]]; then
+        if [ -f /etc/os-release ]; then
+            source /etc/os-release
+            if [[ $ID == "ubuntu" ]]; then
+                INSTALLER="apt-get"
+            elif [[ $ID == "centos" ]]; then
+                INSTALLER="yum"
+            else
+                echo "Unsupported Linux distribution"
+                exit 1
+            fi
+        else
+            echo "Unsupported Linux distribution"
+            exit 1
+        fi
+    elif [[ "$(uname)" == "Darwin" ]]; then
+        INSTALLER="brew"
+    else
+        echo "Unsupported operating system"
+        exit 1
+    fi
 }
 
-link_to_homedir() {
+install_required_commands() {
 
-  echo "backup old dotfiles..."
-  if [ ! -d "$HOME/.dotbackup" ];then
-    echo "$HOME/.dotbackup not found. Auto Make it"
-    mkdir "$HOME/.dotbackup"
-  fi
+    # Add required commands
+    commands=("curl" "git" "zsh")
 
-  local script_dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd -P)"
-  local dotdir=$(dirname ${script_dir})
-  if [[ "$HOME" != "$dotdir" ]];then
-    for f in $dotdir/.??*; do
-
-      [[ `basename $f` == ".git" ]] && continue
-      [[ `basename $f` == ".gitignore" ]] && continue
-
-      if [[ -L "$HOME/`basename $f`" ]];then
-        rm -f "$HOME/`basename $f`"
-      fi
-      if [[ -e "$HOME/`basename $f`" ]];then
-        mv "$HOME/`basename $f`" "$HOME/.dotbackup"
-      fi
-
-      ln -snf $f $HOME
-
+    for cmd in "${commands[@]}"; do
+        if ! command -v "$cmd" &> /dev/null; then
+            echo "Installing $cmd..."
+            sudo "$installer" install -y "$cmd"
+        else
+            echo "$cmd is already installed."
+        fi
     done
-  else
-    echo "same install src dest"
-  fi
+}
+
+setup_environment() {
+
+    echo "backup old dotfiles..."
+    if [ ! -d "$HOME/.dotbackup" ];then
+        echo "$HOME/.dotbackup not found. Auto Make it"
+        mkdir "$HOME/.dotbackup"
+    fi
+
+    local script_dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd -P)"
+    local dotdir=$(dirname ${script_dir})
+    if [[ "$HOME" != "$dotdir" ]];then
+        for f in $dotdir/.??*; do
+
+        [[ `basename $f` == ".git" ]] && continue
+        [[ `basename $f` == ".gitignore" ]] && continue
+
+        if [[ -L "$HOME/`basename $f`" ]];then
+            rm -f "$HOME/`basename $f`"
+        fi
+        if [[ -e "$HOME/`basename $f`" ]];then
+            mv "$HOME/`basename $f`" "$HOME/.dotbackup"
+        fi
+
+        ln -snf $f $HOME
+
+        done
+    else
+        echo "same install src dest"
+    fi
+}
+
+setup_os_specific() {
+    if [ "$INSTALLER" == "brew" ]; then
+        brew bundle --file $HOME/dotfiles/.brew/Brewfile
+    fi
 }
 
 
-has() {
-  type "$1" > /dev/null 2>&1
+main() {
+    set_installer
+    install_required_commands
+    setup_environment
+    setup_os_specific
 }
 
-DOT_DIR="$HOME/dotfiles"
-
-# Auto cloning Omaam's dotfiles
-if [ ! -d ${DOT_DIR} ]; then
-  if has "git"; then
-    git clone https://github.com/Omaam/dotfiles.git ${DOT_DIR}
-  else
-    echo "git required"
-    exit 1
-  fi
-else
-  echo "dotfiles already exists"
-  exit 1
-fi
-
-
-while [ $# -gt 0 ];do
-  case ${1} in
-    --debug|-d)
-      set -uex
-      ;;
-    --help|-h)
-      helpmsg
-      exit 1
-      ;;
-    *)
-      ;;
-  esac
-  shift
-done
-
-link_to_homedir
-git config --global include.path "~/.gitconfig_shared"
-echo "Install completed!"
+# Main実行
+main

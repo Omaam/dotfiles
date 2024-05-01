d9c74fc665ad93683265a684035b4d0f04cec782
Omaam <omama.tomoki@gmail.com>
2024-05-01 23:43:33 +0900

    update install script

--- install.sh
+++ install.sh
@@ -18,20 +18,45 @@ link_to_homedir() {
   local dotdir=$(dirname ${script_dir})
   if [[ "$HOME" != "$dotdir" ]];then
     for f in $dotdir/.??*; do
+
       [[ `basename $f` == ".git" ]] && continue
       [[ `basename $f` == ".gitignore" ]] && continue
+
       if [[ -L "$HOME/`basename $f`" ]];then
         rm -f "$HOME/`basename $f`"
       fi
       if [[ -e "$HOME/`basename $f`" ]];then
         mv "$HOME/`basename $f`" "$HOME/.dotbackup"
       fi
+
       ln -snf $f $HOME
+
     done
   else
     echo "same install src dest"
   fi
 }
+
+
+has() {
+  type "$1" > /dev/null 2>&1
+}
+
+DOT_DIR="$HOME/dotfiles"
+
+# Auto cloning Omaam's dotfiles
+if [ ! -d ${DOT_DIR} ]; then
+  if has "git"; then
+    git clone https://github.com/Omaam/dotfiles.git ${DOT_DIR}
+  else
+    echo "git required"
+    exit 1
+  fi
+else
+  echo "dotfiles already exists"
+  exit 1
+fi
+
 
 while [ $# -gt 0 ];do
   case ${1} in

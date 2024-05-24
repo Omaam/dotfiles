91dd68de47b0ff3ae860454422326c8d8279e603
Omaam <omama.tomoki@gmail.com>
2024-05-24 10:02:04 +0900

    update to more flexible

--- .hammerspoon/toggle.lua
+++ .hammerspoon/toggle.lua
@@ -1,16 +1,33 @@
+function getAppPath(appName)
+  local appPath = "/Applications/" .. appName .. ".app"
+  if not hs.fs.attributes(appPath, "mode") then
+    appPath = "/System/Applications/" .. appName .. ".app"
+  end
+  return appPath
+end
+
+
 function toggleApp(appName, key)
   -- Usage
-  --   toggleApp("<app_name>", "<launch_key>")
+  --   toggleApp("app_name", "launch_key")
   -- Example
-  --   toggleApp("Google Chrome"", "c")
-  hs.hotkey.bind({"control", "Shift"}, key, function()
+  --   toggleApp("Google Chrome", "c")
+  hs.hotkey.bind({"ctrl", "shift"}, key, function()
+
     local app = hs.application.get(appName)
-    if app == nil then
-      hs.application.launchOrFocus("/Applications/" .. appName .. ".app")
+    local appPath = getAppPath(appName)
+
+    if not app then
+      if appPath then
+        hs.application.launchOrFocus(appPath)
+      end
     elseif app:isFrontmost() then
       app:hide()
     else
-      hs.application.launchOrFocus("/Applications/" .. appName .. ".app")
+      -- Some applications cannot be activated by launchOrFocus (e.g., VSCode).
+      if not hs.application.launchOrFocus(appPath) then
+        app:activate()
+      end
     end
   end)
 end

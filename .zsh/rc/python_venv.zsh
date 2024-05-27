44ec6f59801b0d0ac8fb3f640680f409fb1f5b4e
Omaam <omama.tomoki@gmail.com>
2024-05-27 16:13:18 +0900

    delete common activation and update local activation

--- .zsh/rc/python_venv.zsh
+++ .zsh/rc/python_venv.zsh
@@ -1,9 +1,22 @@
 function pyactivate() {
-  if [ -d ".venv" ]; then
-    source .venv/bin/activate
-  else
-    echo ".venv/bin/activate does not exist."
+
+  current_dir=$(pwd)
+
+  # $HOMEディレクトリに到達するまでループ
+  while [ "$current_dir" != "$HOME" ]; do
+      if [ -d "$current_dir/.venv" ]; then
+          break
+      fi
+      current_dir=$(dirname "$current_dir")
+  done
+
+  if [ $current_dir = $HOME ]; then
+    echo ".venv directory not found."
     echo "Please create a virtual environment first."
     return 1
   fi
+
+  echo "source $current_dir/.venv/bin/activate"
+  source "$current_dir/.venv/bin/activate"
+  return 0
 }

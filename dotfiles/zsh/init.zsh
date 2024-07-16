211fc1230642f768d5baf1ddb3057d8e7fceaac1
Omaam <omama.tomoki@gmail.com>
2024-07-16 11:26:15 +0900

    implement lazy load for zsh files

---
+++ dotfiles/zsh/init.zsh
@@ -0,0 +1,32 @@
+for f in `find $HOME/.zsh/essential/*.zsh`; do
+  source $f
+done
+
+function load_shell_files {
+  local option=$1
+  local dir
+
+  case $option in
+    --rc)
+      dir=~/.zsh/rc
+      ;;
+    --local)
+      dir=~/.zsh/local
+      ;;
+    --functions)
+      dir=~/.zsh/functions
+      ;;
+    *)
+      echo "Usage: load_shell_files [--rc|--local|--functions]"
+      return 1
+      ;;
+  esac
+
+  local f
+  for f in $dir/*.zsh; do
+    source $f
+  done
+}
+alias load_rc='load_shell_files --rc'
+alias load_local='load_shell_files --local'
+alias load_functions='load_shell_files --functions'

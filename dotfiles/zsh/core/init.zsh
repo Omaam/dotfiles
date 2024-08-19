33e25abbee53d44338c1de09e250229629eec083
Omaam <omama.tomoki@gmail.com>
2024-08-19 10:35:55 +0900

    Rename 'essential' to 'core'

---
+++ dotfiles/zsh/core/init.zsh
@@ -0,0 +1,29 @@
+function load_shell_files {
+  local option=$1
+  local dir
+
+  case $option in
+    --rc)
+      dir=~/.zsh/rc
+      ;;
+    --local)
+      dir=~/.zsh/local_lazy
+      ;;
+    --functions)
+      dir=~/.zsh/functions
+      ;;
+    *)
+      echo "Usage: load_shell_files [--rc|--local|--functions]"
+      return 1
+      ;;
+  esac
+
+  local f
+  for f in $dir/*.zsh; do
+    source $f
+  done
+}
+alias load_rc='load_shell_files --rc'
+alias load_local='load_shell_files --local'
+alias load_functions='load_shell_files --functions'
+alias ldl='load_local'
---
+++ dotfiles/zsh/core/ohmyzsh.zsh
@@ -0,0 +1,5 @@
+# oh-my-zsh
+ZSH_THEME="robbyrussell"
+plugins=()
+export ZSH="$HOME/.oh-my-zsh"
+source $ZSH/oh-my-zsh.sh
